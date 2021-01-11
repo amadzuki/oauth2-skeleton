7cb4cc41db15227bdc1147ab01f77dd171fe0d6f
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2021-01-11 16:59:14 +0700

    Bootstrap `User` model and migration

---
+++ src/migrations/20210111091536-create-user.js
@@ -0,0 +1,41 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      hash: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: 'updated_at',
+      },
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('users')
+  },
+}
---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      name: { type: DataTypes.STRING, unique: true },
+      username: { type: DataTypes.STRING, unique: true },
+      email: { type: DataTypes.STRING, unique: true },
+      hash: { type: DataTypes.STRING, unique: true },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  )
+  return User
+}

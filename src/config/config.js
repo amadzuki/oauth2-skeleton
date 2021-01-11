653a62ca65ed54df598f631ee806b6903e418ef9
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2021-01-11 15:55:23 +0700

    Sequelize init for project bootstrap

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    migrationStorageTableName: 'sequelize_meta',
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    migrationStorageTableName: 'sequelize_meta',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    migrationStorageTableName: 'sequelize_meta',
+  },
+}
